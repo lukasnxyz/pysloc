05f860bc075a44804661960c21760515c8b18700
Lukas Nitzsche <lukasnitzsche@yahoo.com>
2024-05-04 09:48:15 +0200

    some things working, Commit and Repo obejcts working

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-### Pylesshero
+### pysloc
 An easy way to visualize a git repository's commit log.
 
 ![chart example]()
 
-Fully inspired from [lesshero by kaihendry](https://github.com/kaihendry/lesshero).
+Full credit for the idea comes from [lesshero by kaihendry](https://github.com/kaihendry/lesshero),
+go check it out!
 
 #### Quick-start
 ```shell
@@ -11,10 +12,10 @@ python3 -m venv venv
 source venv/bin/activate/
 pip3 install -r requirements.txt
 ```
-Then run Pylesshero in a git repository or direct it to one
+then run pysloc in a git repository or direct it to one
 ```shell
-python3 pylesshero.py .
+python3 pysloc.py .
 # or
-python3 pylesshero.py -d "dir/of/git/repo"
+python3 pysloc.py -d "dir/of/git/repo"
 ```
-If you want to "install" pylesshero to your system, ...
+If you want to "install" pysloc to your system, ...
---
+++ pysloc.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+from pygit2 import Repository, Signature
+from pygit2.enums import SortMode
+from datetime import datetime, timezone, timedelta
+
+class Commit:
+    def __init__(self, msg: str, author: Signature, commit_hash: str):
+        tzinfo = timezone(timedelta(minutes=author.offset))
+        dt = datetime.fromtimestamp(float(author.time), tzinfo)
+
+        self.msg = msg
+        self.author = author.name
+        self.commit_hash = commit_hash
+        self.time = dt.strftime("%c %z")
+
+        self.sloc_added = 0
+        self.sloc_removed = 0
+        self.sloc_diff = abs(self.sloc_added - self.sloc_removed)
+
+    def __repr__(self):
+        return f"commit {self.commit_hash}\nAuthor:\t{self.author}\nTime:\t{self.time}\n\t{self.msg}"
+
+class Repo:
+    def __init__(self, name: str, path: str, commits: []=[]):
+        self.name = name
+        self.commits = commits
+        self.path = path
+
+    def append_commit(self, commit: Commit):
+        self.commits.append(commit)
+
+    def log(self):
+        for c in self.commits:
+            print(c)
+
+    def __repr__(self):
+        return f"Name: {self.name}\nPath: '{self.path}'\nCommits: {len(self.commits)}"
+
+def main():
+    # handle cli flags here
+
+    pygit_repo = Repository(".git")
+    repo = Repo("pysloc", ".")
+
+    for commit in pygit_repo.walk(pygit_repo.head.target, SortMode.TOPOLOGICAL | SortMode.REVERSE):
+        repo.append_commit(Commit(
+            commit.message, 
+            commit.author, 
+            commit.hex)
+        )
+
+    repo.log()
+    print(repo)
+
+if __name__ == "__main__":
+    main()
